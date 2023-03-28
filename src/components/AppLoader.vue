4cdf5cbf188db98a4855bb0700e9373a8cc8c6ff
KevinZucca <kev.94@live.it>
2023-03-28 18:19:07 +0200

    feat: stylizing loader

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -13,12 +13,49 @@ export default {
 </script>
 
 <template>
-    <h1>Loading...</h1>
+    <div id="container">
+        <div id="container-centered">
+            <h1>Loading...</h1>
+            <img src="/public/images/logo.png" alt="">
+        </div>
+    </div>    
 </template>
 
 <style lang="scss">
-    h1 {
-        margin: 300px auto;
-        font-size: 30px;
+
+#container {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+
+    width: 90%;
+    height: 80vh;
+    background-image: url("public/images/wallpaper.png");
+    border: 2px solid;
+    border-radius: 20px;
+    box-shadow: 15px 15px 30px 20px rgba($color: #0000008f, $alpha: 1.0);
+
+    
+    &-centered {
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        
+        text-align: center;
+
+        h1 {
+            font-size: 70px;
+            text-transform: uppercase;
+        }
+
+        img {
+            width: 800px;
+            pointer-events: none;
+        }
+
+        
     }
+}
+
+    
 </style>
